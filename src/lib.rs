faeef8e25342d7a019429a4fa8887095a9ed4a58
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2023-02-06 21:58:42 +0900

    Add Cargo.toml

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+//pub const VERSION: &str = git_version::git_version!();
+pub const VERSION: &str = "0.1.0";
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+
+    let app = clap::Command::new("datadact")
+        .author("Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>")
+        .version(datadact::VERSION)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .arg(
+            clap::Arg::new("config_path")
+                .long("config")
+                .short('c')
+                //                .takes_value(true)
+                .help("Provide a specific config file"),
+        )
+        .subcommand(
+            clap::Command::new("scheduler")
+                .alias("server")
+                .about("Launch the scheduler process")
+                .after_help("The scheduler has an API server embedded."),
+        );
+
+    let args = app.get_matches();
+
+    match args.subcommand().expect("subcommand is required") {
+        ("scheduler", _args) => {
+            println!("Placeholder");
+        }
+        _ => unreachable!("clap should have already checked the subcommands"),
+    }
+
+    Ok(())
+}
